b8f7521d4be918a1730ef6736804d23ad71d5814
Benjamin Hipple <bhipple@bloomberg.net>
2016-05-29 23:30:13 -0400

    Use regular GHC when Stack not installed (Travis)

--- docs/manual/build.sh
+++ docs/manual/build.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 mkdir -p _shake
-stack ghc -- --make Build.hs -rtsopts -with-rtsopts=-I0 -outputdir=_shake -o _shake/build && _shake/build "$@"
+command -v stack >/dev/null 2>&1
+if [ $? -eq 0 ]; then
+    stack ghc -- --make Build.hs -rtsopts -with-rtsopts=-I0 -outputdir=_shake -o _shake/build && _shake/build "$@"
+else
+    ghc --make Build.hs -rtsopts -with-rtsopts=-I0 -outputdir=_shake -o _shake/build && _shake/build "$@"
+fi
